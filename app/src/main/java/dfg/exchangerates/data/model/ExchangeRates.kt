57b6c8c9fd0c9f109b165fe1f271a0bb5e02ec7c
daniel muncaciu <daniel.muncaciu@endava.com>
2022-02-23 18:40:18 +0200

    cleanup

--- app/src/main/java/dfg/exchangerates/data/model/ExchangeRates.kt
+++ app/src/main/java/dfg/exchangerates/data/model/ExchangeRates.kt
@@ -1,26 +1,23 @@
 package dfg.exchangerates.data.model
 
-import android.provider.Telephony
-import com.google.gson.annotations.SerializedName
-
 object ExchangeRates {
 
     data class  Rates (
-        @SerializedName("rates") val rates: List<Rate>
+        val rates: List<Rate>
     )
 
     data class Rate (
-        @SerializedName("from") val from: String,
-        @SerializedName("to") val to: String,
-        @SerializedName("rate") val rate: String
+        val from: String,
+        val to: String,
+        val rate: String
     )
 
     data class Pairs (
-        @SerializedName("pairs") val pairs: List<Pair>
+        val pairs: List<Pair>
     )
 
     data class Pair (
-        @SerializedName("from") val from: String,
-        @SerializedName("to") val to: String
+        val from: String,
+        val to: String
     )
 }
